1e4f1e456fed2cafa4a6780ca1e10aa76a5bd6df
Diex <diegoalberti@qb9.net>
2017-10-29 12:50:22 -0300

    agrego ejemplos del curso del cceba

--- cceba/ejemplo_MIDI_pwm/ejemplo_MIDI_pwm.ino
+++ cceba/ejemplo_MIDI_pwm/ejemplo_MIDI_pwm.ino
@@ -2,19 +2,22 @@
 
 int led = 9;
 
+// configuración de la librería midi. 
 struct MySettings : public midi::DefaultSettings
 {
     static const unsigned SysExMaxSize = 1024; // Accept SysEx messages up to 1024 bytes long.
     static const bool UseRunningStatus = true; // My devices seem to be ok with it.
 };
 
-// Create a 'MIDI' object using MySettings bound to Serial2.
+// creo un objeto midiin utilizando el Serial1
 MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial1, MIDI, MySettings);
 
 void setup()
 {
     MIDI.begin(MIDI_CHANNEL_OMNI);
-    Serial.begin(115200);
+    // Serial refiere a Serial0 que es siempre la conexion USB
+    Serial.begin(115200); 
+    // callbacks
     MIDI.setHandleNoteOn(handleNoteOn);
     MIDI.setHandleNoteOff(handleNoteOff);
 }
@@ -38,7 +41,7 @@ void handleNoteOn(byte channel, byte note, byte velocity){
 
 void handleNoteOff(byte channel, byte note, byte velocity){
   if(note == 72){
-    Serial.println("off");
+    Serial.println("off note 72");
     analogWrite(led, 255);
   }
 }
